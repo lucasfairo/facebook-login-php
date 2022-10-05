99fbfdc9fe517c729ee629cb6d0138ba921e2d93
Lucas <41524152+lucasfairo@users.noreply.github.com>
2022-10-05 02:39:24 -0300

    Update McryptPseudoRandomStringGenerator.php
    mcrypt_create_iv( )
    This function was DEPRECATED in PHP 7.1.0, and REMOVED in PHP 7.2.0.
    Alternatives to this function include: random_bytes()

--- facebook-login-php/Facebook/PseudoRandomString/McryptPseudoRandomStringGenerator.php
+++ facebook-login-php/Facebook/PseudoRandomString/McryptPseudoRandomStringGenerator.php
@@ -27,42 +27,44 @@ use Facebook\Exceptions\FacebookSDKException;
 
 class McryptPseudoRandomStringGenerator implements PseudoRandomStringGeneratorInterface
 {
-    use PseudoRandomStringGeneratorTrait;
 
-    /**
-     * @const string The error message when generating the string fails.
-     */
-    const ERROR_MESSAGE = 'Unable to generate a cryptographically secure pseudo-random string from mcrypt_create_iv(). ';
+  use PseudoRandomStringGeneratorTrait;
 
-    /**
-     * @throws FacebookSDKException
-     */
-    public function __construct()
-    {
-        if (!function_exists('mcrypt_create_iv')) {
-            throw new FacebookSDKException(
-                static::ERROR_MESSAGE .
-                'The function mcrypt_create_iv() does not exist.'
-            );
-        }
+  /**
+   * @const string The error message when generating the string fails.
+   */
+  const ERROR_MESSAGE = 'Unable to generate a cryptographically secure pseudo-random string from random_bytes(). ';
+
+  /**
+   * @throws FacebookSDKException
+   */
+  public function __construct()
+  {
+    if ( ! function_exists('random_bytes')) {
+      throw new FacebookSDKException(
+        static::ERROR_MESSAGE .
+        'The function random_bytes() does not exist.'
+      );
+    }
+  }
+
+  /**
+   * @inheritdoc
+   */
+  public function getPseudoRandomString($length)
+  {
+    $this->validateLength($length);
+
+    $binaryString = random_bytes($length);
+
+    if ($binaryString === false) {
+      throw new FacebookSDKException(
+        static::ERROR_MESSAGE .
+        'random_bytes() returned an error.'
+      );
     }
 
-    /**
-     * @inheritdoc
-     */
-    public function getPseudoRandomString($length)
-    {
-        $this->validateLength($length);
+    return $this->binToHex($binaryString, $length);
+  }
 
-        $binaryString = mcrypt_create_iv($length, MCRYPT_DEV_URANDOM);
-
-        if ($binaryString === false) {
-            throw new FacebookSDKException(
-                static::ERROR_MESSAGE .
-                'mcrypt_create_iv() returned an error.'
-            );
-        }
-
-        return $this->binToHex($binaryString, $length);
-    }
 }
